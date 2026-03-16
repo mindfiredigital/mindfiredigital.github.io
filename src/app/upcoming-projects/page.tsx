e86a989a824ffb99fee3c503ceef8b885ade5c53
ParasMindfire <mfsi.parasb@outlook.com>
2026-03-16 16:53:04 +0530

    refactor(upcomingProject): move metadata to layout page

--- src/app/upcoming-projects/page.tsx
+++ src/app/upcoming-projects/page.tsx
@@ -1,46 +1,10 @@
 import React from "react";
-import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import projectsImage from "../../../public/images/projects.webp";
 import ProjectGrid from "../projects/components/ProjectGrid";
 import upcomingProjectData from "@/asset/upcomingProjects.json";
-import meta from "../../metadata/metadata.json";
 import { PROJECTS_HERO } from "@/constants";
-
-export const metadata: Metadata = {
-  title: meta["Upcoming-Projects"].title,
-  description: meta["Upcoming-Projects"].description,
-
-  openGraph: {
-    title: meta["Upcoming-Projects"].title,
-    description: meta["Upcoming-Projects"].description,
-    images: {
-      url: meta["Upcoming-Projects"].openGraph.images,
-      height: "627",
-      width: "1200",
-    },
-    url: meta["Upcoming-Projects"].openGraph.url,
-    type: "website",
-    siteName: "Mindfire Digital LLP",
-    locale: "en_US",
-  },
-  twitter: {
-    card: "app",
-    title: meta["Upcoming-Projects"].title,
-    description: meta["Upcoming-Projects"].description,
-    site: "@mindfires",
-    creator: "@mindfires",
-    app: {
-      name: "twitter_app",
-      id: {
-        iphone: "twitter_app://iphone",
-        ipad: "twitter_app://ipad",
-        googleplay: "twitter_app://googleplay",
-      },
-    },
-  },
-};
 
 export default function ProjectsPage() {
   return (
