a411686df4b607611c9fb476a2c3f38cab21c702
ParasMindfire <mfsi.parasb@outlook.com>
2026-03-17 12:02:02 +0530

    feat(about): add metadata in layout file

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import type { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import AboutSegmentSection from "./components/AboutSegmentSection";
@@ -12,41 +11,6 @@ import {
   ABOUT_CONTRIBUTIONS,
 } from "@/constants";
 import aboutUs from "../../../public/images/about-us.webp";
-import meta from "../../metadata/metadata.json";
-
-export const metadata: Metadata = {
-  title: meta["About"].title,
-  description: meta["About"].description,
-
-  openGraph: {
-    title: meta["About"].title,
-    description: meta["About"].description,
-    images: {
-      url: meta["About"].openGraph.images,
-      height: "627",
-      width: "1200",
-    },
-    url: meta["About"].openGraph.url,
-    type: "website",
-    siteName: "Mindfire Digital LLP",
-    locale: "en_US",
-  },
-  twitter: {
-    card: "app",
-    title: meta["About"].title,
-    description: meta["About"].description,
-    site: "@mindfires",
-    creator: "@mindfires",
-    app: {
-      name: "twitter_app",
-      id: {
-        iphone: "twitter_app://iphone",
-        ipad: "twitter_app://ipad",
-        googleplay: "twitter_app://googleplay",
-      },
-    },
-  },
-};
 
 const About = () => {
   return (
