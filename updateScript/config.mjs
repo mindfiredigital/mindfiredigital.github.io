76cbcd0d52ec3094db71a457b0c7ad73dcbc6249
ParasMindfire <mfsi.parasb@outlook.com>
2026-03-18 13:02:33 +0530

    fix(logger): change file name from ts to mjs

--- updateScript/config.mjs
+++ updateScript/config.mjs
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import packagesData from "../src/asset/packages.json" with { type: "json" };
-import logger from "../src/app/utils/logger.ts";
+import logger from "../src/app/utils/logger.mjs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
