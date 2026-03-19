da069034043ff553b7e4ae99d035c65707c88ac0
ParasMindfire <mfsi.parasb@outlook.com>
2026-03-19 15:42:53 +0530

    feat: add sentry to global error handler

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import clientLogger from "@/app/utils/clientLogger";
 import { ERROR } from "@/constants";
+import * as Sentry from "@sentry/nextjs";
 
 export default function GlobalError({
   error,
@@ -13,6 +14,11 @@ export default function GlobalError({
 }) {
   /* Log the root-level error to the server on every error change */
   useEffect(() => {
+    Sentry.captureException(error, {
+      level: "fatal",
+      extra: { digest: error.digest, location: "global-error.tsx" },
+    });
+
     clientLogger.error("Global root-level error caught", {
       message: error.message,
       digest: error.digest,
