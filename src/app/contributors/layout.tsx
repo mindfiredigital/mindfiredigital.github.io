a955d228871f31b33174aea4d3f893580b419490
ParasMindfire <mfsi.parasb@outlook.com>
2026-04-01 17:21:19 +0530

    refactor(contributors): remove constants from metadata

--- src/app/contributors/layout.tsx
+++ src/app/contributors/layout.tsx
@@ -1,41 +1,43 @@
 import React from "react";
 import type { Metadata } from "next";
 import meta from "@/metadata/metadata.json";
+import { CONTRIBUTORS_METADATA_CONSTANTS } from "@/constants";
 
 /*
-  Metadata for "Projects" page
+  Metadata for "Contributors" page
   - Automatically injected into <head> by Next.js
   - Helps improve SEO and social media previews
 */
 export const metadata: Metadata = {
-  title: meta["Contributors"].title,
-  description: meta["Contributors"].description,
+  title: meta[CONTRIBUTORS_METADATA_CONSTANTS.CONTRIBUTORS].title,
+  description: meta[CONTRIBUTORS_METADATA_CONSTANTS.CONTRIBUTORS].description,
 
   openGraph: {
-    title: meta["Contributors"].title,
-    description: meta["Contributors"].description,
+    title: meta[CONTRIBUTORS_METADATA_CONSTANTS.CONTRIBUTORS].title,
+    description: meta[CONTRIBUTORS_METADATA_CONSTANTS.CONTRIBUTORS].description,
     images: {
-      url: meta["Contributors"].openGraph.images,
-      height: "627",
-      width: "1200",
+      url: meta[CONTRIBUTORS_METADATA_CONSTANTS.CONTRIBUTORS].openGraph.images,
+      height: CONTRIBUTORS_METADATA_CONSTANTS.OPEN_GRAPH_IMAGE_HEIGHT,
+      width: CONTRIBUTORS_METADATA_CONSTANTS.OPEN_GRAPH_IMAGE_WIDTH,
     },
-    url: meta["Contributors"].openGraph.url,
+    url: meta[CONTRIBUTORS_METADATA_CONSTANTS.CONTRIBUTORS].openGraph.url,
     type: "website",
-    siteName: "Mindfire Digital LLP",
-    locale: "en_US",
+    siteName: CONTRIBUTORS_METADATA_CONSTANTS.SITE_NAME,
+    locale: CONTRIBUTORS_METADATA_CONSTANTS.LOCALE,
   },
+
   twitter: {
-    card: "app",
-    title: meta["Contributors"].title,
-    description: meta["Contributors"].description,
-    site: "@mindfires",
-    creator: "@mindfires",
+    card: CONTRIBUTORS_METADATA_CONSTANTS.TWITTER_CARD_TYPE,
+    title: meta[CONTRIBUTORS_METADATA_CONSTANTS.CONTRIBUTORS].title,
+    description: meta[CONTRIBUTORS_METADATA_CONSTANTS.CONTRIBUTORS].description,
+    site: CONTRIBUTORS_METADATA_CONSTANTS.TWITTER_SITE,
+    creator: CONTRIBUTORS_METADATA_CONSTANTS.TWITTER_CREATOR,
     app: {
-      name: "twitter_app",
+      name: CONTRIBUTORS_METADATA_CONSTANTS.TWITTER_APP_NAME,
       id: {
-        iphone: "twitter_app://iphone",
-        ipad: "twitter_app://ipad",
-        googleplay: "twitter_app://googleplay",
+        iphone: CONTRIBUTORS_METADATA_CONSTANTS.TWITTER_APP_ID_IPHONE,
+        ipad: CONTRIBUTORS_METADATA_CONSTANTS.TWITTER_APP_ID_IPAD,
+        googleplay: CONTRIBUTORS_METADATA_CONSTANTS.TWITTER_APP_ID_GOOGLEPLAY,
       },
     },
   },
